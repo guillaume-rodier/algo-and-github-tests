8c10012d7cc8e0e30ab316a7c40eb2dabf25b56d
Guillaume <guillaume.rodier1992@gmail.com>
2026-01-12 15:49:29 +0100

    Reinit exercice 2 for github user search

--- exercise-2-github-user-search/src/tests/FizzBuzz.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest"
-import { fizzbuzz } from "@/utils/fizzbuzz"
-
-describe("fizzbuzz", () => {
-  it("First test", () => {
-    expect(fizzbuzz(3)).toBe("Fizz")
-  })
-})
